e5d280c1da936cbdd2f3ad5c3eb72c2068e0fa3c
Héctor O'Higgins <guille_081195@outloook.com>
2024-10-18 20:18:17 -0500

    carga inicial de proyecto

---
+++ src/main/java/pe/edu/cibertec/demo_azure/controller/VehiculoController.java
@@ -0,0 +1,30 @@
+package pe.edu.cibertec.demo_azure.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class VehiculoController {
+
+    @GetMapping("/vehiculo")
+    public String obtenerVehiculos(Model model) {
+        List<String> listaVehiculos = new ArrayList<String>();
+
+        listaVehiculos.add("Toyota");
+        listaVehiculos.add("Honda");
+        listaVehiculos.add("Mclaren");
+        listaVehiculos.add("Nissan");
+        listaVehiculos.add("Suzuki");
+
+        model.addAttribute("vehiculos", listaVehiculos);
+        return "index";
+
+    }
+
+
+}
